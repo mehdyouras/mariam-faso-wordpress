52a93e211152a949ed72ffeb5aa1135ce9a851e3
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-18 10:15:17 +0200

    Setting up wordpress

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <h2 aria-hidden="true">Pied de page</h2>
+    <section class="footer__item footer-partners">
+        <h3>Nos partenaires</h3>
+        <ul class="o-list-inline">
+            <li class="footer-partners__item"><a href="https://www.kbs-frb.be/"><img src="<?php mf_asset('img/fondation-roi-baudouin.png'); ?>" alt="Logo de la Fondation Roi Baudouin"></a></li>
+            <li class="footer-partners__item"><a href="http://www.federation-wallonie-bruxelles.be/"><img src="<?php mf_asset('img/wallonie-bruxelles.png'); ?>" alt="Logo de la Fédération Wallonie-Bruxelles"></a></li>
+        </ul>
+    </section>
+    <section class="footer__item footer-contact">
+        <h3><a class="footer-contact__contact-link" href="#">Nous contacter</a></h3>
+        <address class="footer-contact__content"><span class="footer-contact__content-item">Mariam-Faso ASBL</span><span class="footer-contact__content-item">Association sociale</span><span class="footer-contact__content-item">Rue de la gare, 15</span><span class="footer-contact__content-item">6600 BASTOGNE</span><span class="footer-contact__content-item footer-contact__content-item_icon-phone">+32 4 79 21 57 44</span><span class="footer-contact__content-item footer-contact__content-item_icon-email">info@mariam-faso.be</span></address>
+    </section>
+    <script src="<?php mf_asset('js/script.js'); ?>"></script>
+</footer>
+</section>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php mf_asset('css/main.css'); ?>">
+    <link rel="icon" href="img/favicon.ico">
+    <title>Accueil - Mariam-Faso</title>
+</head>
+<body>
+<header class="header <?php if(!is_home()) :?>header_smal<?php endif; ?>">
+    <div class="site-header">
+        <h1 class="site-title"> <a class="site-title" href="index.html"><img class="site-title__logo" src="<?php mf_asset('img/logo.png'); ?>" alt"Logo de Mariam-Faso">Mariam-Faso</a></h1>
+        <a class="hamburger-menu" href="#"></a>
+        <nav class="site-nav">
+            <h2 aria-hidden="true">Navigation principale</h2>
+            <a class="site-nav__close-btn" href="#">&times;</a>
+            <ul class="o-list-bare site-nav__list">
+                <li class="site-nav__item active"><a class="site-nav__link" href="#">Accueil</a></li>
+                <li class="site-nav__item"><a class="site-nav__link" href="projects.html">Projets</a></li>
+                <li class="site-nav__item site-nav__item_dropdown">
+                    <a class="site-nav__link site-nav__link_dropdown" href="agenda.html" tabindex="-1">Agenda</a>
+                    <ul class="o-list-bare site-nav__dropdown">
+                        <li class="site-nav__dropdown-item"><a class="site-nav__dropdown-link" href="actions.html">Actions en cours</a></li>
+                        <li class="site-nav__dropdown-item"><a class="site-nav__dropdown-link" href="events.html">Evenements</a></li>
+                    </ul>
+                </li>
+                <li class="site-nav__item"><a class="site-nav__link" href="#">À propos</a></li>
+                <li class="site-nav__item site-nav__item_cta-container"><a class="site-nav__link cta cta_dark cta_no-border" href="#">Faire un don</a><a class="site-nav__link cta cta_dark cta_no-border" href="#">Participer</a></li>
+            </ul>
+        </nav>
+    </div>
+    <section class="home-motivational">
+        <h2 class="home-motivational__title"><span class="home-motivational__item">Dévelopmment,</span> <span class="home-motivational__item">Echange,</span> <span class="home-motivational__item">Solidarité,</span> <span class="home-motivational__item">Education.</span></h2>
+        <a class="cta cta_centered cta_over-image" href="#">Découvrir nos projets</a>
+    </section>
+</header>
