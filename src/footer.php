b9bbc077833c5fc650934a7ab31a7bfc45826d62
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-20 14:29:57 +0200

    Redone footer design and added content

--- src/footer.php
+++ src/footer.php
@@ -1,18 +1,45 @@
 <footer class="footer">
     <h2 aria-hidden="true">Pied de page</h2>
-    <section class="footer__item footer-partners">
-        <h3>Nos partenaires</h3>
-        <ul class="o-list-inline">
-            <li class="footer-partners__item"><a href="https://www.kbs-frb.be/"><img src="<?php mf_asset('img/kbs_logo.svg'); ?>" alt="Logo de la Fondation Roi Baudouin"></a></li>
-            <li class="footer-partners__item"><a href="http://www.federation-wallonie-bruxelles.be/"><img src="<?php mf_asset('img/fwb_logo.svg'); ?>" alt="Logo de la Fédération Wallonie-Bruxelles"></a></li>
-        </ul>
+    <div class="footer__container">
+        <section class="footer__item footer-partners">
+            <h3>Nos partenaires</h3>
+            <ul class="o-list-inline">
+                <li class="footer-partners__item"><a href="https://www.kbs-frb.be/"><img src="<?php mf_asset('img/kbs_logo.svg'); ?>" alt="Logo de la Fondation Roi Baudouin"></a></li>
+                <li class="footer-partners__item"><a href="http://www.federation-wallonie-bruxelles.be/"><img src="<?php mf_asset('img/fwb_logo.svg'); ?>" alt="Logo de la Fédération Wallonie-Bruxelles"></a></li>
+            </ul>
+        </section>
+        <section class="footer__item footer-donation">
+            <h3><a class="footer-donation__donation-link cta cta_no-border cta_title" href="#">Faire un don</a></h3>
+            <dl class="footer-donation__list">
+                <dt class="footer-donation__dt">Numéro de compte&nbsp;:</dt>
+                <dd class="footer-donation__dd">BE 10 000 0000 0404</dd>
+                <dt class="footer-donation__dt">Communication structurée&nbsp;:</dt>
+                <dd class="footer-donation__dd">128/2338/00037</dd>
+            </dl>
+        </section>
+        <section class="footer__item footer-project">
+            <h3><a class="footer-project__contact-link cta cta_no-border cta_title" href="#">Nos derniers projets</a></h3>
+            <ol class="o-list-bare">
+                <li><a class="footer-project__link" href="#">Projet 1</a></li>
+                <li><a class="footer-project__link" href="#">Projet 2</a></li>
+                <li><a class="footer-project__link" href="#">Projet 3</a></li>
+            </ol>
+            <a href="#" class="cta">Participer</a>
+        </section>
+        <section class="footer__item footer-contact">
+            <h3><a class="footer-contact__contact-link cta cta_no-border cta_title" href="#">Nous contacter</a></h3>
+            <address class="footer-contact__content"><span class="footer-contact__content-item">Mariam-Faso ASBL</span><span class="footer-contact__content-item">Association sociale</span><span class="footer-contact__content-item">Rue de la gare, 15</span><span class="footer-contact__content-item">6600 BASTOGNE</span><span class="footer-contact__content-item footer-contact__content-item_icon-phone">+32 4 79 21 57 44</span><span class="footer-contact__content-item footer-contact__content-item_icon-email">info@mariam-faso.be</span></address>
+        </section>
+    </div>
+    <section class="footer__social">
+        <a class="footer__social-link" title="Facebook" href="#"><span class="footer__social-text">Facebook</span></a>
+        <a class="footer__social-link" title="Youtube" href="#"><span class="footer__social-text">Youtube</span></a>
     </section>
-    <section class="footer__item footer-contact">
-        <h3><a class="footer-contact__contact-link cta cta_no-border cta_title" href="#">Nous contacter</a></h3>
-        <address class="footer-contact__content"><span class="footer-contact__content-item">Mariam-Faso ASBL</span><span class="footer-contact__content-item">Association sociale</span><span class="footer-contact__content-item">Rue de la gare, 15</span><span class="footer-contact__content-item">6600 BASTOGNE</span><span class="footer-contact__content-item footer-contact__content-item_icon-phone">+32 4 79 21 57 44</span><span class="footer-contact__content-item footer-contact__content-item_icon-email">info@mariam-faso.be</span></address>
+    <section class="footer__copyright">
+        <small>&copy; 2017 Mehdy Ouras</small>
     </section>
-    <script src="<?php mf_asset('js/script.js'); ?>"></script>
 </footer>
+<script src="<?php mf_asset('js/script.js'); ?>"></script>
 </section>
 </body>
 </html>
