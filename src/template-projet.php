f53e2b25fa7f483524c56086c07379a06fdb5d3e
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-24 16:11:55 +0200

    Finished loop

--- src/template-projet.php
+++ src/template-projet.php
@@ -17,19 +17,36 @@
             </div>
             <h2 class="section-header__title">Nos projets</h2>
             <p class="section-header__intro"><?php the_field('projects_intro'); ?></p>
+            <a class="cta cta_dark cta_no-border">Faire un don</a>
+            <a class="cta cta_dark cta_no-border">Participer</a>
         </div>
     </header>
     <div class="projects content-wrapper">
-        <article class="project-excerpt">
-            <header class="project-excerpt__header">
-                <h3 class="project-excerpt__title"><a class="project-excerpt__link" href="#">Le nom d'un projet</a></h3><a class="cta cta_dark">Participer</a>
-            </header>
-            <div class="project-excerpt__content"><img class="project-excerpt__thumbnail" src="img/article1.jpg" alt="Photo du projet">
-                <div class="project-excerpt__descriptions-container">
-                    <p class="project-excerpt__description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium blanditiis vel iste laudantium, quidem minus ipsa, quas nostrum nulla aliquid nemo nihil accusantium ratione, quia sit tempora totam architecto eos.</p>
-                    <p class="project-excerpt__description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque cupiditate voluptate nemo obcaecati, necessitatibus cumque. Distinctio dicta nulla tempora perspiciatis recusandae at sequi dolore officiis. Totam excepturi provident facere!</p><a class="read-more" href="#">Lire plus</a>
-                </div>
-            </div>
-        </article>
+    <?php
+        $args = array( 'post_type' => 'project', 'posts_per_page' => 10 );
+        $loop = new WP_Query( $args );
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                <article class="project-excerpt">
+                    <header class="project-excerpt__header">
+                        <h3 class="project-excerpt__title"><a class="project-excerpt__link" href="<?php the_permalink(); ?>"><?php the_field('project_name'); ?></a></h3>
+                        <?php if(get_field('project_startdate')) :?>
+                            <p class="project-excerpt__info">
+                                <span class="project-excerpt__date">
+                                    <time <?= mf_get_datetime(get_field('project_startdate')); ?>><?php the_field('project_startdate'); ?></time>
+                                    <?php if(get_field('project_enddate')) :?>au <time datetime="<?= mf_get_datetime(get_field('project_enddate')); ?>"><?php the_field('project_enddate'); ?></time><?php endif; ?>
+                                </span>
+                                <span class="project-excerpt__location"><?php the_field('project_location'); ?></span>
+                            </p>
+                        <?php endif; ?>
+
+                    </header>
+                    <div class="project-excerpt__content"><img class="project-excerpt__thumbnail" src="<?php the_field('project_thumbnail'); ?>" alt="Photo du projet">
+                        <div class="project-excerpt__descriptions-container">
+                            <p class="project-excerpt__description"><?php the_field('project_excerpt'); ?></p>
+                            <a class="read-more" href="<?php the_permalink(); ?>">En savoir plus</a>
+                        </div>
+                    </div>
+                </article>
+        <?php endwhile; ?>
     </div>
 <?php get_footer(); ?>
