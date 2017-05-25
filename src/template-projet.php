268d0872446b040357e01176472b06a717127197
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-25 16:03:53 +0200

    Modified project display order

--- src/template-projet.php
+++ src/template-projet.php
@@ -23,29 +23,36 @@
     </header>
     <div class="projects content-wrapper">
     <?php
-        $args = array( 'post_type' => 'project', 'posts_per_page' => 10 );
+        $args = array( 'post_type' => 'project',
+            'posts_per_page' => -1,
+            'meta_key'			=> 'project_startdate',
+            'orderby'			=> 'meta_value',
+            'order'				=> 'DESC'
+            );
         $loop = new WP_Query( $args );
         while ( $loop->have_posts() ) : $loop->the_post(); ?>
                 <article class="project-excerpt">
-                    <header class="project-excerpt__header">
-                        <h3 class="project-excerpt__title"><a class="project-excerpt__link" href="<?php the_permalink(); ?>"><?php the_field('project_name'); ?></a></h3>
-                        <?php if(get_field('project_startdate')) :?>
-                            <p class="project-excerpt__info">
+                    <div class="project-excerpt__content-wrapper">
+                        <header class="project-excerpt__header">
+                            <h3 class="project-excerpt__title"><a class="project-excerpt__link" href="<?php the_permalink(); ?>"><?php the_field('project_name'); ?></a></h3>
+                            <?php if(get_field('project_startdate')) :?>
+                                <p class="project-excerpt__info">
                                 <span class="project-excerpt__date">
                                     <time <?= mf_get_datetime(get_field('project_startdate')); ?>><?php the_field('project_startdate'); ?></time>
                                     <?php if(get_field('project_enddate')) :?>au <time datetime="<?= mf_get_datetime(get_field('project_enddate')); ?>"><?php the_field('project_enddate'); ?></time><?php endif; ?>
                                 </span>
-                                <span class="project-excerpt__location"><?php the_field('project_location'); ?></span>
-                            </p>
-                        <?php endif; ?>
+                                    <span class="project-excerpt__location"><?php the_field('project_location'); ?></span>
+                                </p>
+                            <?php endif; ?>
 
-                    </header>
-                    <div class="project-excerpt__content"><img class="project-excerpt__thumbnail" src="<?php the_field('project_thumbnail'); ?>" alt="Photo du projet">
-                        <div class="project-excerpt__descriptions-container">
-                            <p class="project-excerpt__description"><?php the_field('project_excerpt'); ?></p>
-                            <a class="read-more" href="<?php the_permalink(); ?>">En savoir plus</a>
+                        </header>
+                        <div class="project-excerpt__content"><img class="project-excerpt__thumbnail" src="<?php the_field('project_thumbnail'); ?>" alt="Photo du projet">
+                            <div class="project-excerpt__descriptions-container">
+                                <p class="project-excerpt__description"><?php the_field('project_excerpt'); ?></p>
+                            </div>
                         </div>
                     </div>
+                    <a class="read-more read-more_force-right" href="<?php the_permalink(); ?>">En savoir plus</a>
                 </article>
         <?php endwhile; ?>
     </div>
