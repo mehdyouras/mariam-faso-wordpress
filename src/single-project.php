8ad4160c43804c29a0a7882d02fa8279fde6103c
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-25 17:54:50 +0200

    Added first markup for flexible content

---
+++ src/single-project.php
@@ -0,0 +1,69 @@
+<?php
+get_header();
+?>
+
+<section class="wrapper">
+    <header class="section-header section-header_projects">
+        <div class="section-header__content-container">
+            <div class="section-header__breadcrumb">
+                <?php
+                if(function_exists('bcn_display'))
+                {
+                    bcn_display();
+                }
+                ?>
+            </div>
+            <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+            <h2 class="section-header__title"><?php the_title(); ?></h2>
+            <?php if(get_field('project_startdate')) :?>
+            <p class="project-excerpt__info section-header__intro">
+                <span class="project-excerpt__date">
+                    <time <?= mf_get_datetime(get_field('project_startdate')); ?>><?php the_field('project_startdate'); ?></time>
+                    <?php if(get_field('project_enddate')) :?>au <time datetime="<?= mf_get_datetime(get_field('project_enddate')); ?>"><?php the_field('project_enddate'); ?></time><?php endif; ?>
+                </span>
+            <?php endif; ?>
+                <span class="project-excerpt__location"><?php the_field('project_location'); ?></span>
+            </p>
+
+        </div>
+    </header>
+    <section class="project content-wrapper">
+        <?php
+        if( have_rows('projet') ): ?>
+            <?php while ( have_rows('projet') ) : the_row(); ?>
+
+                <?php if( get_row_layout() == 'project_text' ): ?>
+                    <div class="wysiwyg">
+                        <?php the_sub_field('project_text_wysiwyg'); ?>
+                    </div>
+                <?php elseif( get_row_layout() == 'project_image_full-width' ):?>
+                    <div class="image_full-width">
+                        <figure>
+                            <img src="<?php the_sub_field('project_image_image'); ?>" alt="">
+                            <figcaption>
+                                <?php the_sub_field('project_image_caption'); ?>
+                            </figcaption>
+                        </figure>
+                    </div>
+                <?php elseif( get_row_layout() == 'project_text-image' ):?>
+                    <div class="text-image text-image<?php the_sub_field('project_text-image_leftright') ?>">
+                        <div class="wysiwyg">
+                            <?php the_sub_field('project_text-image_text'); ?>
+                        </div>
+                        <figure>
+                            <img src="<?php the_sub_field('project_text-image_image'); ?>" alt="">
+                            <figcaption><?php the_sub_field('project_text-image_caption'); ?></figcaption>
+                        </figure>
+                    </div>
+                <?php elseif( get_row_layout() == 'project_text-image' ):?>
+                    
+                <?php endif; ?>
+
+            <?php endwhile; ?>
+
+        <?php endif; ?>
+    </section>
+    <?php endwhile; ?>
+    <?php endif; ?>
+    <?php get_footer(); ?>
