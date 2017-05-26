1afebba6044dda3f625683e323ac920853f853f7
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-05-26 15:10:03 +0200

    Working on styles

--- src/single-project.php
+++ src/single-project.php
@@ -39,23 +39,25 @@ get_header();
                             <?php the_sub_field('project_text_wysiwyg'); ?>
                         </div>
                     </div>
+
                 <?php elseif( get_row_layout() == 'project_image_full-width' ):?>
                     <div class="image_full-width">
                         <figure>
                             <img src="<?php the_sub_field('project_image_image'); ?>" alt="">
-                            <figcaption>
+                            <figcaption class="figure__caption">
                                 <?php the_sub_field('project_image_caption'); ?>
                             </figcaption>
                         </figure>
                     </div>
+
                 <?php elseif( get_row_layout() == 'project_text-image' ):?>
                     <div class="text-image text-image<?php the_sub_field('project_text-image_leftright') ?>">
                         <div class="wysiwyg">
                             <?php the_sub_field('project_text-image_text'); ?>
                         </div>
-                        <figure class="text-image__image">
-                            <img src="<?php the_sub_field('project_text-image_image'); ?>" alt="">
-                            <figcaption><?php the_sub_field('project_text-image_caption'); ?></figcaption>
+                        <figure class="text-image__figure">
+                            <img class="figure__image" src="<?php the_sub_field('project_text-image_image'); ?>" alt="">
+                            <figcaption class="figure__caption"><?php the_sub_field('project_text-image_caption'); ?></figcaption>
                         </figure>
                     </div>
                 <?php endif; ?>
