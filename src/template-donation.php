af70f8ebf4ee178cb08240799c7e8f274700cc23
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-03 10:58:05 +0200

    Added first markup for donation content

--- src/template-donation.php
+++ src/template-donation.php
@@ -25,8 +25,24 @@ get_header();
         </div>
     </header>
     <section class="post content-wrapper">
-
+        <h2 class="u-hidden-visually">Réaliser un don</h2>
+        <section>
+            <h3>Les avantages fiscaux d'une donation</h3>
+            <div class="wysiwyg">
+                <?php the_field('donation_fisc') ?>
+            </div>
+            <a href="<?php the_field('donation_spf') ?>" class="cta cta_dark">En savoir plus sur les avantages fiscaux liés aux donations</a>
+        </section>
+        <section>
+            <h3>Réaliser un don via la Fondation Roi Baudouin</h3>
+            <dl class="footer-donation__list">
+                <dt class="footer-donation__dt">Numéro de compte&nbsp;:</dt>
+                <dd class="footer-donation__dd"><?php the_field('iban_frb', 'option');?></dd>
+                <dt class="footer-donation__dt">Communication structurée&nbsp;:</dt>
+                <dd class="footer-donation__dd"><?php the_field('com_frb', 'option');?></dd>
+            </dl>
+            <a class="cta cta_dark" href="<?php the_field('donation_frb'); ?>">Réaliser un don à Mariam Faso via la Fondation Roi Baudouin</a>
+        </section>
     </section>
-
 
 <?php get_footer(); ?>
