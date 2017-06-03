11afad375e74a88ecab7d7170ee857814cad164f
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-03 09:44:29 +0200

    Fixed homepage link

--- src/header.php
+++ src/header.php
@@ -11,7 +11,7 @@
 <body>
 <header class="header <?php if(!is_front_page()) :?>header_small<?php endif; ?>">
     <div class="site-header">
-        <h1 class="site-title"><a class="site-title__link" href="index.html"><img class="site-title__logo" src="<?php mf_asset('img/logo.svg'); ?>" alt"Logo de Mariam-Faso"><span class="site-title__content">Marriam-Faso</span></a></h1>
+        <h1 class="site-title"><a class="site-title__link" href="<?= get_site_url(); ?>"><img class="site-title__logo" src="<?php mf_asset('img/logo.svg'); ?>" alt"Logo de Mariam-Faso"><span class="site-title__content">Marriam-Faso</span></a></h1>
         <a class="hamburger-menu" href="#"></a>
         <?php get_template_part('part', 'navbar');?>
     </div>
