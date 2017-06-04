0f81220b858098bda5d58de5caade9cdb9603e74
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-04 20:58:47 +0200

    Added loop for actions

--- src/part-actions.php
+++ src/part-actions.php
@@ -1,3 +1,8 @@
+<?php
+    $actions = get_page_by_title('actions');
+    $actions = $actions->ID;
+?>
+
 <header class="section-header section-header_actions">
     <div class="section-header__content-container">
         <div class="section-header__breadcrumb">
@@ -8,37 +13,37 @@
             }
             ?>
         </div>
-        <h2 class="section-header__title">Les actions en cours</h2>
-        <p class="section-header__intro">Les différentes actions que nous organisons tout au long de l'année nous permette de récolter du materiels, des vètements, des jouets <i>etc</i>. Afin de les redistribuer lors de nos prochains voyages.</p>
+        <h2 class="section-header__title"><?= __('Les actions en cours','mf');?></h2>
+        <p class="section-header__intro"><?php the_field('actions_intro', $actions); ?></p>
         <a class="cta cta_dark cta_no-border" href="<?php mf_the_permalink_by_title("projets") ?>">Nos projets</a>
         <a class="cta cta_dark cta_no-border" href="<?php mf_the_permalink_by_title("participer") ?>">Participer</a>
     </div>
 </header>
 <section class="excerpt-container content-wrapper">
+    <?php
+    $args = array( 'post_type' => 'action',
+        'posts_per_page' => -1,
+        'meta_key'			=> 'action_startdate',
+        'orderby'			=> 'meta_value',
+        'order'				=> 'DESC'
+    );
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post(); ?>
     <article class="post-excerpt">
-        <header class="post-excerpt__header">
-            <h3 class="post-excerpt__title">Collecte de jouets</h3>
-            <p class="post-excerpt__info">
+        <div class="post-excerpt__content-wrapper">
+            <header class="post-excerpt__header">
+                <h3 class="post-excerpt__title"><?php the_title(); ?></h3>
+                <p class="post-excerpt__info">
                     <span class="post-excerpt__date">
-                        Du <time class="actions__time">22/06</time> au <time class="actions__time">22/09</time>.
+                        Du <time datetime="<?php mf_the_datetime(get_field('action_startdate')); ?>" class="actions__time"><?php the_field('action_startdate') ?></time> au <time datetime="<?php mf_the_datetime(get_field('action_enddate')); ?>" class="actions__time"><?php the_field('action_enddate') ?></time>.
                     </span>
-                <span class="post-excerpt__location">Rue Neuve 5, Huy.</span>
-            </p>
-        </header>
-        <div class="post-excerpt__content">
-            <p class="actions__description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga nobis unde, distinctio voluptates beatae molestiae, maxime esse corporis perspiciatis nihil deleniti accusamus temporibus voluptas placeat hic similique ad iusto nostrum!</p>
+                    <span class="post-excerpt__location"><?php the_field('action_location') ?></span>
+                </p>
+            </header>
+            <div class="post-excerpt__content">
+                <?php the_field('action_description') ?>
+            </div>
         </div>
     </article>
-    <article class="post-excerpt">
-        <header class="post-excerpt__header">
-            <h3 class="post-excerpt__title">Collecte de jouets</h3>
-            <p class="post-excerpt__date">Du
-                <time class="actions__time">22/06</time> au
-                <time class="actions__time">22/09</time>.
-            </p>
-        </header>
-        <div class="post-excerpt__content">
-            <p class="actions__description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga nobis unde, distinctio voluptates beatae molestiae, maxime esse corporis perspiciatis nihil deleniti accusamus temporibus voluptas placeat hic similique ad iusto nostrum!</p>
-        </div>
-    </article>
+    <?php endwhile; ?>
 </section>
