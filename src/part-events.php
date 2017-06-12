6ab521d82ed4190b75d3a2b48c105c6804390310
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-12 21:38:50 +0200

    Fixed typo

--- src/part-events.php
+++ src/part-events.php
@@ -31,25 +31,29 @@ $events = $events->ID;
     $loop = new WP_Query( $args );
     while ( $loop->have_posts() ) : $loop->the_post(); ?>
         <article itemscope itemtype="http://schema.org/Event" role="article" class="post-excerpt">
-            <header class="post-excerpt__header">
-                <h3 itemprop="name" aria-level=3 class="post-excerpt__title"><a class="post-excerpt__link" href="<?php the_permalink(); ?>"><?php the_field('event_title'); ?></a></h3>
-                <p class="post-excerpt__info">
+            <div>
+                <header class="post-excerpt__header">
+                    <h3 itemprop="name" aria-level=3 class="post-excerpt__title"><a class="post-excerpt__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <p class="post-excerpt__info">
                     <span class="post-excerpt__date">
                         <time itemprop="startDate" datetime="<?= mf_get_datetime(get_field('event_date')); ?><?php if(get_field('event_time') !== ''): echo 'T'; the_field('event_time'); endif; ?>"><?php the_field('event_date'); ?><?php if(get_field('event_time')): ?><?= __(' à ','mf'); ?><?php the_field('event_time'); ?><?php endif; ?></time>
                     </span>
-                    <span itemprop="location" itemscope itemtype="http://schema.org/Place" class="post-excerpt__location">
+                        <span itemprop="location" itemscope itemtype="http://schema.org/Place" class="post-excerpt__location">
                         <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
                             <span itemprop="streetAddress"><?php the_field('event_location'); ?></span>
                         </span>
                     </span>
-                    <a href="<?php the_field('event_fb'); ?>"><span class="post-excerpt__fb"><?= __("Vers l'événement facebook",'mf');?></span></a>
-                </p>
-            </header>
-            <div class="post-excerpt__content">
-                <?php mf_the_image(get_field('event_thumbnail'), "mf_thumbnail", "post-excerpt__thumbnail"); ?>
-                <p itemprop="description" class="post-excerpt__description"><?php the_field('event_excerpt'); ?></p>
+                        <?php if(get_field('event_fb')) : ?>
+                        <a href="<?php the_field('event_fb'); ?>"><span class="post-excerpt__fb"><?= __("Vers l'événement facebook",'mf');?></span></a>
+                        <?php endif; ?>
+                    </p>
+                </header>
+                <div class="post-excerpt__content">
+                    <?php mf_the_image(get_field('event_thumbnail'), "mf_thumbnail", "post-excerpt__thumbnail"); ?>
+                    <p itemprop="description" class="post-excerpt__description"><?php the_field('event_excerpt'); ?></p>
+                </div>
             </div>
-            <a class="cta cta_dark cta_no-border cta_force-right" href="<?php the_permalink(); ?>"><?= __('En savoir plus','mf');?><span class="u-hidden-visually" aria-hidden="true"><?= __(" sur l'événement ",'mf');?></span><?php the_title(); ?></a>
+            <a class="cta cta_dark cta_no-border cta_force-right" href="<?php the_permalink(); ?>"><?= __('En savoir plus','mf');?><span class="u-hidden-visually" aria-hidden="true"><?= __(" sur l'événement ",'mf');?><?php the_title(); ?></span></a>
         </article>
     <?php endwhile; ?>
 </div>
